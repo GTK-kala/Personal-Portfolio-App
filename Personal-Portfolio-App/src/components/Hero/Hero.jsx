738a9cf1a9bcfb7108f97b8a33f7b309105d7ae9
GTK-kala <gtkkala1110@gmail.com>
2025-11-23 21:39:15 +0300

    Adding Dark mood in the Hero section

--- Personal-Portfolio-App/src/components/Hero/Hero.jsx
+++ Personal-Portfolio-App/src/components/Hero/Hero.jsx
@@ -1,25 +1,29 @@
+import { ContextApi } from "../../context/ContextProvider";
 import { Data } from "../../assets/Data/Data";
+import { useContext } from "react";
 import "./Hero.css";
 
 const Hero = () => {
+  const { theme } = useContext(ContextApi);
+
   return (
-    <section className="hero" id="home">
+    <section className={theme ? "hero_light" : "hero_night"} id="home">
       {/* Floating shapes */}
       <div className="floating-shape shape"></div>
 
       <div className="hero-content">
-        <h1 className="hero-title">
+        <h1 className={theme ? "hero-title_light" : "hero-title_night"}>
           Hi, I'm <span>Khalid</span>
         </h1>
-        <p className="hero-subtitle">
+        <p className={theme ? "hero-subtitle_light" : "hero-subtitle_night"}>
           Frontend Developer | React.js Enthusiast | Creating Modern Web
           Experiences
         </p>
         <div className="hero-buttons">
-          <a href="/resume.pdf" className="btn" download>
+          <a href="/resume.pdf" className={theme ? "btn_light" : "btn_night"} download>
             Download CV
           </a>
-          <a href="#contact" className="btn btn-secondary">
+          <a href="#contact" className={theme ? "btn_light " : "btn_night"}>
             Contact Me
           </a>
         </div>
