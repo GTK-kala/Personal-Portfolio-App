e4dc72d3a35c19988cc7c8bcfaf21c9b9baae9c0
GTK-kala <gtkkala1110@gmail.com>
2025-11-26 20:58:48 +0300

    Add loader component with animation and integrate into app

--- Personal-Portfolio-App/src/App.jsx
+++ Personal-Portfolio-App/src/App.jsx
@@ -1,11 +1,29 @@
-import Home from './Page/Home';
+import Loader from "./components/Loader/Loader";
+import { useState, useEffect } from "react";
+import Home from "./Page/Home";
 
 const App = () => {
+  
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <>
-      <Home />
+      { loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Home />
+        </>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ Personal-Portfolio-App/src/components/Loader/Loader.jsx
@@ -0,0 +1,13 @@
+import "./Loader.css";
+
+const Loader = () => {
+  return (
+    <div className="loader-container">
+      <div className="bounce"></div>
+      <div className="bounce"></div>
+      <div className="bounce"></div>
+    </div>
+  );
+};
+
+export default Loader;
